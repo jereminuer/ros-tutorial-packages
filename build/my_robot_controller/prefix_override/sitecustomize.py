04efc0e127411ee9894646fd3a6fc041cdb707f5
Jeremi <jereminuerofficial@gmail.com>
2023-06-27 14:48:46 -0700

    pls work

--- build/my_robot_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jeremi/ros2_ws/src/install/my_robot_controller'
--- build/urdf_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jeremi/ros2_ws/src/install/urdf_controller'
